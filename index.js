a28a8104acbc4495e1385974f8d098d54a9b332d
daiki0381 <daiki03810218@gmail.com>
2022-05-19 23:24:22 +0900

    動くコードの完成

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { prompt } = require('enquirer')
+const player = require('node-wav-player')
+const fs = require('fs');
+
+(async () => {
+  const firstResponse = await prompt({
+    type: 'select',
+    name: 'purpose',
+    message: 'Please select your purpose',
+    choices: ['Chill', 'Relaxing', 'Working']
+  })
+
+  const files = fs.readdirSync(firstResponse.purpose).map(file => file.replace(/.mp3/g, ''))
+
+  const secondResponse = await prompt({
+    type: 'select',
+    name: 'music',
+    message: 'Please select music',
+    choices: files
+  })
+
+  player.play({ path: `${firstResponse.purpose}/${secondResponse.music}.mp3` })
+})()
